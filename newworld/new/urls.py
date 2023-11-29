25743fb18822a2306b3790e3d4d35c0c90e695c0
solomon jesse <statosco@gmail.com>
2023-11-29 08:03:52 +0300

    Initial commit

---
+++ newworld/new/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
+
+
+
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/',views.createRoom, name='create-room'),
+    path('profile/<str:pk>/',views.userProfile, name='userProfile'),
+
+    path('update-room/<str:pk>/',views.updatedRoom, name='update-room'),
+    path('delete-room/<str:pk>/',views.deleteRoom, name='delete-room'),
+    path('delete_meassage/<str:pk>/', views.delete_meassage, name='delete_meassage'),
+
+    path('update-user/', views.updateUser, name='update-user'),
+    path('topics/', views.topicsPage, name='topics'),
+]
