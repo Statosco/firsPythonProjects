f3927c6b9154a5ec07b58c96bb92629a6c912f17
solomon jesse <statosco@gmail.com>
2023-11-29 09:09:51 +0300

    initial commit

--- newworld/newworld/urls.py
+++ newworld/newworld/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('new.urls'))
+    path('', include('new.urls')),
+    path('api/', include('new.api.urls')),
 ]
