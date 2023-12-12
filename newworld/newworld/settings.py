ed4ea96ea6aa6039c5d5399c99e2f4fb53ca5903
solomon jesse <statosco@gmail.com>
2023-12-12 10:33:40 +0300

    initial commit

--- newworld/newworld/settings.py
+++ newworld/newworld/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "new.apps.NewConfig",
 
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "corsheaders.middleware.corseMiddleware",
 ]
 
 ROOT_URLCONF = "newworld.urls"
@@ -130,3 +133,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOW_ALL_ORIGINS = True
